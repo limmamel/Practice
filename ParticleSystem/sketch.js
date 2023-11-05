1a6b041378e07b845ad941e3e4bec442ed6c2ee7
limmamel <limmm0427@naver.com>
2023-11-05 18:04:23 +0900

    BasicParticleSystem

--- ParticleSystem/sketch.js
+++ ParticleSystem/sketch.js
@@ -1,7 +1,59 @@
+class Particle {
+  constructor(x, y) {
+    this.position = createVector(x, y);
+    this.velocity = createVector(random(-1, 1), random(-2, 0));
+    this.acceleration = createVector(0, 0.05);
+    this.lifespan = 255; // 입자 수명
+  }
+
+  update() {
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+    this.lifespan -= 2;
+  }
+
+  display() {
+    stroke(0, this.lifespan);
+    fill(0, this.lifespan);
+    ellipse(this.position.x, this.position.y, 10, 10);
+  }
+
+  isDead() {
+    return this.lifespan < 0;
+  }
+}
+
+class ParticleSystem {
+  constructor(x, y) {
+    this.origin = createVector(x, y);
+    this.particles = [];
+  }
+
+  addParticle() {
+    this.particles.push(new Particle(this.origin.x, this.origin.y));
+  }
+
+  run() {
+    for (let i = this.particles.length - 1; i >= 0; i--) {
+      const particle = this.particles[i];
+      particle.update();
+      particle.display();
+      if (particle.isDead()) {
+        this.particles.splice(i, 1);
+      }
+    }
+  }
+}
+
+let system;
+
 function setup() {
   createCanvas(400, 400);
+  system = new ParticleSystem(width / 2, height);
 }
 
 function draw() {
-  background(220);
+  background(255);
+  system.addParticle();
+  system.run();
 }

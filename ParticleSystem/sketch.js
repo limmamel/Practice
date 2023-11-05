334b1ee151ba9433faf0e2ca091a716cb4bbaf71
limmamel <limmm0427@naver.com>
2023-11-05 18:29:55 +0900

    Add Z_3D

--- ParticleSystem/sketch.js
+++ ParticleSystem/sketch.js
@@ -1,9 +1,9 @@
 class Particle {
-  constructor(x, y) {
-    this.position = createVector(x, y);
-    this.velocity = createVector(random(-1, 1), random(-2, 0));
-    this.acceleration = createVector(0, 0.05);
-    this.lifespan = 255; 
+  constructor(x, y, z) {
+    this.position = createVector(x, y, z);
+    this.velocity = createVector(random(-1, 1), random(-2, 0), random(-1, 1));
+    this.acceleration = createVector(0, 0.05, 0);
+    this.lifespan = 255; // 입자 수명
     this.color = color(random(255), random(255), random(255));
     this.size = random(5, 20);
   }
@@ -26,13 +26,13 @@ class Particle {
 }
 
 class ParticleSystem {
-  constructor(x, y) {
-    this.origin = createVector(x, y);
+  constructor(x, y, z) {
+    this.origin = createVector(x, y, z);
     this.particles = [];
   }
 
   addParticle() {
-    this.particles.push(new Particle(this.origin.x, this.origin.y));
+    this.particles.push(new Particle(this.origin.x, this.origin.y, this.origin.z));
   }
 
   run() {
@@ -50,12 +50,14 @@ class ParticleSystem {
 let system;
 
 function setup() {
-  createCanvas(400, 400);
-  system = new ParticleSystem(width / 2, height);
+  createCanvas(400, 400, WEBGL);
+  system = new ParticleSystem(0, 0, 0);
   background(255);
 }
 
 function draw() {
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
   system.addParticle();
   system.run();
 }
